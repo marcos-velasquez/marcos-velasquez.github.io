6887de61b720d244cf645188e5fdd37fa2753469
Marcos Velásquez <marcos_marquitox@hotmail.com>
2024-05-02 22:32:14 -0400

    feat: add  site: 'https://marcos-velasquez.github.io'

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({ integrations: [tailwind()] });
+export default defineConfig({
+  site: "https://marcos-velasquez.github.io",
+  integrations: [tailwind()],
+});
